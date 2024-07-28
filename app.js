7151bc67e529d6c49187f45dcd8dc2a797b3dc88
ifaxec <xecfahim.ahmed@gmail.com>
2024-07-28 13:56:12 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-import { user1, user2 } from './tweets.js';
+import { user1 } from './tweets.js';
+
+const profileContainer = document.getElementById('container');
 
 
-function displayTweets(user) {
-    console.log(`Tweets by ${user.displayName} (${user.userName}):`);
-    user.tweets.forEach(tweet => {
-        console.log(`[${tweet.timestamp}] ${tweet.text}`);
-    });
-}
+profileContainer.innerHTML = `
 
-displayTweets(user1);
-displayTweets(user2);
+<div>
+    <h2>${user1.displayName}</h2>
+</div>
+
+<div>
+    <img src="${user1.coverPhotoURL}" alt="background image">
+</div>
+
+<div class="profile-info">
+    <h3>Joined ${user1.displayName}</h3>
+    <p>${user1.userName}</p>
+    <p>${user1.joinedDate}</p>
+</div>
+
+<div class="profile-stats">
+    <p>${user1.followingCount} Following</p>
+    <p>${user1.followerCount} Followers</p>
+</div>
+
+<div class="profile-columns">
+    <p>Tweets</p>
+    <p>Tweets & replies</p>
+    <p>Media</p>
+    <p>Likes</p>
+</div>
+
+</div>
+
+
+
+
+`
+
+
+
 
